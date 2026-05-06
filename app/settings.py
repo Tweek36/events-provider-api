c526d4d5f1bd1413e7b7016fbd93ae557bf38d49
tweek36 <sowakov.yura@gmail.com>
2026-05-06 05:32:23 +0300

    feat: Set default values for settings in the Settings class

--- app/settings.py
+++ app/settings.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 from pydantic import ConfigDict, HttpUrl
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    REDIS_URL: str
-    EVENTS_PROVIDER_API_URL: HttpUrl
-    X_API_KEY: str
-    HOSTNAME: str
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:admin@localhost:5433/events_db"
+    REDIS_URL: str = "redis://localhost:6379/0"
+    EVENTS_PROVIDER_API_URL: HttpUrl = HttpUrl("https://api.events-provider.com")
+    X_API_KEY: str = "your-api-key"
+    HOSTNAME: str = "localhost"
 
     model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
 
