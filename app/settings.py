e78b33e8ffa74aa2615a58f40b2025c87974f2ed
tweek36 <sowakov.yura@gmail.com>
2026-05-06 08:19:03 +0300

    refactor: Simplify start script by removing wait loops and consolidating commands

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,6 @@ class Settings(BaseSettings):
     X_API_KEY: str = "your-api-key"
     HOSTNAME: str = "localhost"
 
-    model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 settings = Settings()
